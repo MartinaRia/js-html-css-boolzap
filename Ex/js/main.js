3fcfef75325cff4dcdced46feb9d9825424b126b
Martina <martina.ria@hotmail.com>
2020-04-07 20:19:18 +0200

    Submit message ok
      $('.text-container').hover() non funziona

--- Ex/js/main.js
+++ Ex/js/main.js
@@ -6,12 +6,21 @@ $( document ).ready(function() {
     function showSubmitIcon(){
       $('.invia .fa-microphone').hide();
       $('.invia .fa-paper-plane').show();
-    }
+    };
 
     function hideSubmitIcon(){
       $('.invia .fa-paper-plane').hide();
       $('.invia .fa-microphone').show();
-    }
+    };
+
+    //per azione 3 e altro
+    /*function getTheTime(selectorOra, selectorMinuti){
+      var ora = new Date().getHours();
+      var minuti = new Date().getMinutes();
+
+      selectorOra.html(ora);
+      selectorMinuti.html(minuti);
+    };*/
 
     //AZIONI ----------------------------------------------------------
     //1. apparizione e sparizione icona aeroplano
@@ -31,27 +40,30 @@ $( document ).ready(function() {
 
 
     //2. apparizione icona-info con hover text-container
+    $('.icona-info').hide();
     $('.text-container').hover(
       function(){
-        $('.icona-info').fadeToggle('fast', 'linear')
+        $('.icona-info', this).fadeToggle('fast', 'linear');
       },
       function(){
-        $('.icona-info').fadeToggle('fast', 'linear')
+        $('.icona-info', this).fadeToggle('fast', 'linear');
       }
-    )
+    );
 
 
     // 3. inserisci messaggio al click dell'aeroplano
-    // VARIABILI
 
-
-    // EVENTO
     $('.invia .fa-paper-plane').click(
       function(){
         var messaggio = $('#typing-msg').val();
-        $('.testo-messaggio').html(messaggio)
+        var ora = new Date().getHours();
+        var minuti = new Date().getMinutes();
+
+        $('.tread').append('<div class="messaggio-utente"> <div class="text-container"> <p class="testo-messaggio">' + messaggio + '</p> <p class="message-time"> <span class="hour">' + ora + '</span>' + ':' + '<span class="minute">' + minuti + '</span> </p> <div class="icona-info"> <i class="fas fa-chevron-down"></i> <div class="info"> </div> </div> </div> </div>');
+        $('#typing-msg').val('');
+
       }
-    )
+    );
 
 
 
