a786d43cf5e5071732b52bb673153279e67f4cac
Martina <martina.ria@hotmail.com>
2020-04-07 19:13:49 +0200

    html + css ok (js base)

--- Ex/js/main.js
+++ Ex/js/main.js
@@ -1,7 +1,58 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    console.log( "tutto bene fin ora!" );
+
+    //FUNZIONI --------------------------------------------------------
+    // per azione 1
+    function showSubmitIcon(){
+      $('.invia .fa-microphone').hide();
+      $('.invia .fa-paper-plane').show();
+    }
+
+    function hideSubmitIcon(){
+      $('.invia .fa-paper-plane').hide();
+      $('.invia .fa-microphone').show();
+    }
+
+    //AZIONI ----------------------------------------------------------
+    //1. apparizione e sparizione icona aeroplano
+    $('#typing-msg').focusin(
+      showSubmitIcon
+    );
+
+    $('#typing-msg').focusout(
+      function(){
+        if ($(this).val() == '') {
+          hideSubmitIcon()
+        } else {
+          showSubmitIcon()
+        }
+      }
+    );
+
+
+    //2. apparizione icona-info con hover text-container
+    $('.text-container').hover(
+      function(){
+        $('.icona-info').fadeToggle('fast', 'linear')
+      },
+      function(){
+        $('.icona-info').fadeToggle('fast', 'linear')
+      }
+    )
+
+
+    // 3. inserisci messaggio al click dell'aeroplano
+    // VARIABILI
+
+
+    // EVENTO
+    $('.invia .fa-paper-plane').click(
+      function(){
+        var messaggio = $('#typing-msg').val();
+        $('.testo-messaggio').html(messaggio)
+      }
+    )
 
 
 
-    
 });
